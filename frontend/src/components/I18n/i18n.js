7265179abbd3f83b2a694b96f12dc9ff99d580dc
Musicminion <zhangziqian@sjtu.edu.cn>
2023-07-12 10:41:33 +0800

    i18 in setting tab

--- frontend/src/components/I18n/i18n.js
+++ frontend/src/components/I18n/i18n.js
@@ -27,5 +27,6 @@ i18n
             escapeValue: false, // not needed for react as it escapes by default
         },
     });
+    
 
 export default i18n;
--- frontend/src/components/Tabs/settings.js
+++ frontend/src/components/Tabs/settings.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { Layout, Button, Card, Popconfirm, Divider, Col, Row, Typography, message, InputNumber} from 'antd';
+import { Layout, Button, Card, Popconfirm, Divider, Col, Row, Typography, message, InputNumber, Select} from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
+import { useTranslation } from 'react-i18next';
 import './style.css'
 import { request } from "../../services/request";
 import { getSettings, updateSettings } from "../../services/user";
+import { GlobalOutlined } from '@ant-design/icons';
+import i18n from '../../components/I18n/i18n'
 
 const { Header, Content } = Layout;
 const { Title } = Typography;
 
-function TabSettings ({ onCloseTab }) {
+function TabSettings({ onCloseTab }) {
 
     const [loaded, setLoaded] = useState(true);
     const [settings, setSettings] = useState(null);
@@ -20,12 +23,12 @@ function TabSettings ({ onCloseTab }) {
 
     //获取设置项
     const fetchSettings = async () => {
-        try{
+        try {
             const data = await getSettings();
             setSettings(data);
         } catch (error) {
             console.error('Failed to fetch settings:', error);
-            message.error('请求失败',2);
+            message.error('请求失败', 2);
         }
     };
 
@@ -50,7 +53,7 @@ function TabSettings ({ onCloseTab }) {
             window.location.href = newUrl;
         } catch (error) {
             console.error('Failed to delete sessions:', error);
-            message.error('请求失败',2);
+            message.error('请求失败', 2);
         }
     };
 
@@ -60,61 +63,109 @@ function TabSettings ({ onCloseTab }) {
             window.location.reload();
         } catch (error) {
             console.error('Failed to delete account:', error);
-            message.error('请求失败',2);
+            message.error('请求失败', 2);
         }
     };
 
-    return(
-        <Layout style={{ height: '100%'}}>
+    const LoadLanguage = () => {
+        if (i18n.language === 'zh') {
+            return 'zh-CN';
+        } else if (i18n.language === 'en') {
+            return 'en-US';
+        }
+        return 'zh-CN';
+    }
+
+    const handleLanguageChange = (value) => {
+        if (value === 'zh-CN') {
+            i18n.changeLanguage('zh');
+        }
+        else if (value === 'en-US') {
+            i18n.changeLanguage('en');
+        }
+    }
+
+
+    return (
+        <Layout style={{ height: '100%' }}>
             <Header className='Header' style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 <h2>偏好设置</h2>
-                <Button icon={<CloseOutlined />} onClick={onCloseTab}/>
+                <Button icon={<CloseOutlined />} onClick={onCloseTab} />
             </Header>
-            <Content className={loaded ? 'tab-content float-up' : 'tab-content'} style={{ overflow: 'auto'}}>
+            <Content className={loaded ? 'tab-content float-up' : 'tab-content'} style={{ overflow: 'auto' }}>
                 <Typography>
-                    <Title level={4} style={{marginTop:'25px'}}>模型参数</Title>
-                    <Card style={{marginTop: '25px'}} >
+                    <Title level={4} style={{ marginTop: '25px' }}>语言设置</Title>
+                    <Card style={{ marginTop: '25px' }} >
+                        <Row>
+                            <Col span={12} className="setting-title">
+                                <div><span>语言</span></div>
+                                <div>选择您的界面语言</div>
+                            </Col>
+                            <Col span={12} className="setting-item">
+                                <Select
+                                    defaultValue={LoadLanguage()}
+                                    // style={{
+                                    //     width: 120,
+                                    // }}
+                                    onChange={handleLanguageChange}
+                                    options={[
+                                        {
+                                            value: 'zh-CN',
+                                            label: <><GlobalOutlined /> &nbsp;&nbsp;中文</>,
+                                        },
+                                        {
+                                            value: 'en-US',
+                                            label: <><GlobalOutlined /> &nbsp;&nbsp;English</>,
+                                        },
+                                    ]}
+                                />
+                            </Col>
+                        </Row>
+                    </Card>
+
+                    <Title level={4} style={{ marginTop: '25px' }}>模型参数</Title>
+                    <Card style={{ marginTop: '25px' }} >
                         <Row>
                             <Col span={12} className="setting-title">
                                 <div><span>随机性（temperature）</span></div>
                                 <div>值越大，回复越随机</div>
                             </Col>
                             <Col span={12} className="setting-item">
-                            <InputNumber min={0} max={1} precision={1} step={0.1}
-                                value={settings?.temperature}
-                                onChange={(value) => {handleChangeSettings({ temperature: value });}}
-                            />
+                                <InputNumber min={0} max={1} precision={1} step={0.1}
+                                    value={settings?.temperature}
+                                    onChange={(value) => { handleChangeSettings({ temperature: value }); }}
+                                />
                             </Col>
                         </Row>
-                        <Divider className="setting-divider"/>
+                        <Divider className="setting-divider" />
                         <Row>
                             <Col span={12} className="setting-title">
                                 <div><span>单次回复限制（max_tokens）</span></div>
                                 <div>单次交互所用的最大 Token 数</div>
                             </Col>
                             <Col span={12} className="setting-item">
-                            <InputNumber min={0} max={1000} step={100}
-                                value={settings?.max_tokens}
-                                onChange={(value) => {handleChangeSettings({ max_tokens: value });}}
-                            />
+                                <InputNumber min={0} max={1000} step={100}
+                                    value={settings?.max_tokens}
+                                    onChange={(value) => { handleChangeSettings({ max_tokens: value }); }}
+                                />
                             </Col>
                         </Row>
-                        <Divider className="setting-divider"/>
+                        <Divider className="setting-divider" />
                         <Row>
                             <Col span={12} className="setting-title">
                                 <div><span>附带历史消息数</span></div>
                                 <div>每次请求携带的历史消息数</div>
                             </Col>
                             <Col span={12} className="setting-item">
-                            <InputNumber min={0} max={8}
-                                value={settings?.attached_message_count}
-                                onChange={(value) => {handleChangeSettings({ attached_message_count: value });}}
-                            />
+                                <InputNumber min={0} max={8}
+                                    value={settings?.attached_message_count}
+                                    onChange={(value) => { handleChangeSettings({ attached_message_count: value }); }}
+                                />
                             </Col>
                         </Row>
                     </Card>
-                    <Title level={4} style={{marginTop:'25px'}}>高风险</Title>
-                    <Card style={{marginTop: '25px'}} >
+                    <Title level={4} style={{ marginTop: '25px' }}>高风险</Title>
+                    <Card style={{ marginTop: '25px' }} >
                         <Row>
                             <Col span={12} className="setting-title">
                                 <div><span>清除会话</span></div>
@@ -125,14 +176,14 @@ function TabSettings ({ onCloseTab }) {
                                     title="确认"
                                     description="确认清除所有会话、消息数据？"
                                     onConfirm={handleDeleteAllSessions}
-                                    okText="确定" okButtonProps={{size: 'middle'}}
-                                    cancelText="取消" cancelButtonProps={{size: 'middle'}}
+                                    okText="确定" okButtonProps={{ size: 'middle' }}
+                                    cancelText="取消" cancelButtonProps={{ size: 'middle' }}
                                 >
                                     <Button danger>立即清除</Button>
                                 </Popconfirm>
                             </Col>
                         </Row>
-                        <Divider className="setting-divider"/>
+                        <Divider className="setting-divider" />
                         <Row>
                             <Col span={12} className="setting-title">
                                 <div><span>重置账户</span></div>
@@ -143,8 +194,8 @@ function TabSettings ({ onCloseTab }) {
                                     title="确认"
                                     description="确认重置账户的所有数据？"
                                     onConfirm={handleDeleteAccount}
-                                    okText="确定" okButtonProps={{size: 'middle'}}
-                                    cancelText="取消" cancelButtonProps={{size: 'middle'}}
+                                    okText="确定" okButtonProps={{ size: 'middle' }}
+                                    cancelText="取消" cancelButtonProps={{ size: 'middle' }}
                                 >
                                     <Button danger>立即重置</Button>
                                 </Popconfirm>
@@ -165,5 +216,5 @@ function TabSettings ({ onCloseTab }) {
         </Layout>
     )
 };
-  
+
 export default TabSettings;

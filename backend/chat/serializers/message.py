76931d1c00fbef3b61c34372c876f7cfe1f2f482
Yiyang Wu <toolmanp@outlook.com>
2023-07-16 17:06:21 +0800

    feat(serializer): update serializer settings
    Add Session and Message serializer settings
    
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

---
+++ backend/chat/serializers/message.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from chat.models import Message
+
+class MessageSerializer(serializers.ModelSerializer):
+    time = serializers.DateTimeField(
+            format='%Y/%m/%d %H:%M:%S',
+            source='timestamp')
+    class Meta:
+        model = Message
+        fields = [
+            'sender',
+            'content',
+            'flag_qcmd',
+            'use_model',
+            'time'
+            ]

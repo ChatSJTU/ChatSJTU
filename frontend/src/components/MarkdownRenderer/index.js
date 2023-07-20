3e56699bf45b8d9f545807ee42d6b95a416c17bf
LittleHugeUnicorn <2641712741@qq.com>
2023-07-20 16:55:22 +0800

    feat(frontend): add codeblock render in markdown renderer

--- frontend/src/components/MarkdownRenderer/index.js
+++ frontend/src/components/MarkdownRenderer/index.js
@@ -6,17 +6,77 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex'
 import remarkHtml from 'remark-html';
 import rehypeRaw from 'rehype-raw'
+import copy from 'copy-to-clipboard';
+import { Button, message, Popconfirm, Space } from 'antd';
+import { CopyOutlined, CodeOutlined } from '@ant-design/icons';
+import DOMPurify from 'dompurify'
 
 import 'katex/dist/katex.min.css';
 import 'github-markdown-css/github-markdown-light.css';
 
+//复制
+const handleCopy = (content) => {
+    copy(content);
+    message.success('代码已复制到剪贴板', 2);
+};
+
+//运行HTML
+const handleRun = (code) => {
+    const sanitizedCode = DOMPurify.sanitize(code, {ALLOWED_TAGS: []}); 
+    
+    let blob = new Blob([`"use strict";\n${sanitizedCode}`], {type: 'text/javascript'});
+    let url = URL.createObjectURL(blob);
+
+    const newWindow = window.open();
+    newWindow.document.write('<scr' + 'ipt src="' + url + '"><\/scr' + 'ipt>');
+}
+      
+const CodeBlock = {
+
+    code({node, inline, className, children, ...props}) {
+    const match = /language-(\w+)/.exec(className || '')
+        return !inline && match
+        ? <div style={{position: 'relative'}}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                {<div style={{ fontSize: '14px', color: '#ccc', textTransform: 'uppercase', letterSpacing: '1px' }}>
+                    {match[1]}
+                </div>}
+                <Space>
+                    { match[1] === 'html' &&
+                        <Popconfirm
+                            title="确认"
+                            description="此操作会唤起新页面运行，请自行确保HTML代码的安全性"
+                            onConfirm={() => handleRun(children[0])}
+                            okText="确定" okButtonProps={{ size: 'middle' }}
+                            cancelText="取消" cancelButtonProps={{ size: 'middle' }}
+                        >
+                            <Button style={{ color: '#aaa' }} size='small' type='text' icon={<CodeOutlined />}>
+                                运行
+                            </Button>
+                        </Popconfirm>
+                    }
+                    <Button
+                        style={{ color: '#aaa' }} size='small' type='text' icon={<CopyOutlined/>}
+                        onClick={() => handleCopy(children[0])}>
+                        复制
+                    </Button>
+                </Space>
+            </div>
+            <pre className={className} style={{fontSize:'14px', margin:'0px', padding:'5px 0px'}}>
+                {children[0]}
+            </pre>
+        </div>
+        : <code className={className} {...props}>
+            {children}
+        </code>
+    }
+};
+
 const MarkdownRenderer = ({content}) =>{
 
     const renderers = {
-        // inlineMath: ({value}) => <InlineMath>{value}</InlineMath>,
-        // math: ({value}) => <BlockMath>{value}</BlockMath>,
-        // html: ({value}) => <div dangerouslySetInnerHTML={{ __html: value }} />
-    }
+        ...CodeBlock,
+    };
 
     return(
         <ReactMarkdown

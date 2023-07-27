19a1e77e87753d5d429808777a4b8830580dd0b8
Yiyang Wu <toolmanp@outlook.com>
2023-07-27 20:44:33 +0800

    feat(admin): initial admin site configuration
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from chat.models import Message, UserAccount
 
 # Register your models here.
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('session', 'sender', 'use_model', 'timestamp')
+    date_hierarchy = 'timestamp'
+    
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+@admin.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'usage_count', 'last_used')
+
+    def has_change_permission(self, request, obj=None):
+        return False

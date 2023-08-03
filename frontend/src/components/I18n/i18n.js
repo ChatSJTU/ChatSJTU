2d84ec5983a41221283c80b151b9bca0fcc2090f
Ichigo2315 <csy_ichigo@sjtu.edu.cn>
2023-08-03 19:09:37 +0800

    feat(i18n): finished i18n for Tabs/about, Tabs/wallet & Tabs/disclaimer

--- frontend/src/components/I18n/i18n.js
+++ frontend/src/components/I18n/i18n.js
@@ -1,17 +1,33 @@
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import i18n from "i18next";                     // i18n 的主要模块
+
 import LoginLayout_zh from '../../locales/zh-cn/LoginLayout.json';
+import Tabs_about_zh from '../../locales/zh-cn/Tabs_about.json';
+import Tabs_disclaimer_zh from '../../locales/zh-cn/Tabs_disclaimer.json';
+import Tabs_wallet_zh from '../../locales/zh-cn/Tabs_wallet.json';
+
 import LoginLayout_en from '../../locales/en-us/LoginLayout.json';
+import Tabs_about_en from '../../locales/en-us/Tabs_about.json';
+import Tabs_disclaimer_en from '../../locales/en-us/Tabs_disclaimer.json';
+import Tabs_wallet_en from '../../locales/en-us/Tabs_wallet.json';
+
 import { initReactI18next } from 'react-i18next'; 
 
 
 const i18nResources = {
     en: {
-        LoginLayout: LoginLayout_en
+        LoginLayout: LoginLayout_en,
+        Tabs_about: Tabs_about_en,
+        Tabs_disclaimer: Tabs_disclaimer_en,
+        Tabs_wallet: Tabs_wallet_en,
+        
     },
     zh: {
-        LoginLayout: LoginLayout_zh
+        LoginLayout: LoginLayout_zh,
+        Tabs_about: Tabs_about_zh,
+        Tabs_disclaimer: Tabs_disclaimer_zh,
+        Tabs_wallet: Tabs_wallet_zh,
     }
 }
 

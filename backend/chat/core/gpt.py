7836f6d260bb5a1daefb24636e2cf07556f8bb2c
Yiyang Wu <toolmanp@outlook.com>
2023-11-25 00:34:13 +0800

    fix(gpt): fix import error
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

--- backend/chat/core/gpt.py
+++ backend/chat/core/gpt.py
@@ -4,7 +4,7 @@ from .plugins.fc import FCSpec
 from .configs import CHAT_MODELS
 from .errors import ChatError
 
-from typing import Awaitable, Callable, Union, Self
+from typing import Awaitable, Callable, Union
 from dataclasses import asdict, dataclass
 from abc import ABC, abstractmethod
 import functools
@@ -12,6 +12,7 @@ import tenacity
 import logging
 import dacite
 import openai
+import os
 
 logger = logging.getLogger(__name__)
 
